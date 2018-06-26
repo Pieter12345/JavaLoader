ebc5a3fc9add5568421623c8e36e421234586ff1
Pieter12345 <P.J.S.Kools@student.tudelft.nl>
2018-06-26 05:22:54 +0200

    Finallize BukkitCommand class + Require non-null command name
    - Finallize the BukkitCommand class so that JavaLoader users cannot extend from this and inject their implementation into JavaLoader.
    - Require non-null command names since commands without a name do not make sense.
    - Fix NullPointerException in the second constructor when aliases is null.

--- src/main/java/io/github/pieter12345/javaloader/bukkit/BukkitCommand.java
+++ src/main/java/io/github/pieter12345/javaloader/bukkit/BukkitCommand.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
+import java.util.Objects;
 
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.TabCompleter;
@@ -13,7 +14,7 @@ import org.bukkit.command.TabCompleter;
  * Represents a command on a Bukkit server.
  * @author P.J.S. Kools
  */
-public class BukkitCommand {
+public final class BukkitCommand {
 	
 	// Variables & Constants.
 	private final String name;
@@ -29,15 +30,22 @@ public class BukkitCommand {
 	 * Constructor.
 	 * Creates a new BukkitCommand with the given parameters.
 	 * @param name - The name of the command ("mycommand" would be triggered by "/mycommand" in-game).
-	 * @param description - The command description.
+	 * @param description - The command description or null to supply no description.
 	 * @param usageMessage - The command usage message.
+	 * Supplying an empty string or null will cause no usage message to be set.
 	 * @param permission - The required permission to use this command.
+	 * Supplying an empty string or null will cause everyone to have permission to execute this command.
 	 * @param aliases - A list of aliases for the command.
+	 * Supplying an empty list or null will cause no aliases to be set.
 	 * @param executor - The CommandExecutor which will be used to handle the command.
+	 * Supplying null will cause no command executor to be set.
 	 * @param completer - The TabCompleter which will be used to handle TAB-completions for the command.
+	 * Supplying null will cause no tabcompleter to be set.
+	 * @throws NullPointerException When name is null.
 	 */
 	public BukkitCommand(String name, String description, String usageMessage, String permission,
-			List<String> aliases, CommandExecutor executor, TabCompleter completer) {
+			List<String> aliases, CommandExecutor executor, TabCompleter completer) throws NullPointerException {
+		Objects.requireNonNull(name, "Name may not be null.");
 		this.name = name.toLowerCase(Locale.ENGLISH);
 		this.description = description;
 		this.usageMessage = usageMessage;
@@ -52,16 +60,23 @@ public class BukkitCommand {
 	 * Constructor.
 	 * Creates a new BukkitCommand with the given parameters.
 	 * @param name - The name of the command ("mycommand" would be triggered by "/mycommand" in-game).
-	 * @param description - The command description.
+	 * @param description - The command description or null to supply no description.
 	 * @param usageMessage - The command usage message.
+	 * Supplying an empty string or null will cause no usage message to be set.
 	 * @param permission - The required permission to use this command.
+	 * Supplying an empty string or null will cause everyone to have permission to execute this command.
 	 * @param aliases - An array of aliases for the command.
+	 * Supplying an empty array or null will cause no aliases to be set.
 	 * @param executor - The CommandExecutor which will be used to handle the command.
+	 * Supplying null will cause no command executor to be set.
 	 * @param completer - The TabCompleter which will be used to handle TAB-completions for the command.
+	 * Supplying null will cause no tabcompleter to be set.
+	 * @throws NullPointerException When name is null.
 	 */
 	public BukkitCommand(String name, String description, String usageMessage, String permission,
-			String[] aliases, CommandExecutor executor, TabCompleter completer) {
-		this(name, description, usageMessage, permission, Arrays.asList(aliases), executor, completer);
+			String[] aliases, CommandExecutor executor, TabCompleter completer) throws NullPointerException {
+		this(name, description, usageMessage, permission,
+				(aliases == null ? null : Arrays.asList(aliases)), executor, completer);
 	}
 	
 	/**
